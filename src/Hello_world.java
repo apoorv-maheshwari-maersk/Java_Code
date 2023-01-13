849c684a877963172db9304c333226623fb614e3
apoorv-maheshwari-maersk <apoorv.maheshwari@maersk.com>
2023-01-14 02:26:01 +0530

    Input

--- src/Hello_world.java
+++ src/Hello_world.java
@@ -1,5 +1,12 @@
 public class Hello_world {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+    private String name;
+     public void setName(String name) {
+         this.name = name;
+     }
+
+public String getName(){
+    return name;
+}
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+        Hello_world myAccount = new Hello_world();
+        System.out.printf("Initial name is: %s%n%n", myAccount.getName());
+
+        System.out.println("please enter the name:");
+        String theName = input.nextLine();
+        myAccount.setName(theName);
+        System.out.println();
+
+        System.out.printf("Name in object myAccount is: %n%s%n", myAccount.getName());
     }
 }
