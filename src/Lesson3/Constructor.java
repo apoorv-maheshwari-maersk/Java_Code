86ab35469646de4159156dbe69c31a80ed232989
apoorv-maheshwari-maersk <apoorv.maheshwari@maersk.com>
2023-01-16 02:20:17 +0530

    Constructor

---
+++ src/Lesson3/Constructor.java
@@ -0,0 +1,19 @@
+package Lesson3;
+
+public class Constructor {
+
+   private String name;
+
+   public Constructor(String name) {
+      this.name = name;
+   }
+
+   public void setName(String name){
+      this.name =  name;
+   }
+
+   public String getName(){
+      return name;
+   }
+}
+
