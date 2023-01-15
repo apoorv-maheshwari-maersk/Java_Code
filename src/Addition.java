7e9704cd610a07420bfd44ba28c0126e080d4175
apoorv-maheshwari-maersk <apoorv.maheshwari@maersk.com>
2023-01-15 23:33:06 +0530

    Addition

---
+++ src/Addition.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Addition {
+   public static void main(String[] arg){
+      Scanner input = new Scanner (System.in);
+
+      int number1;
+      int number2;
+      int sum;
+
+      System.out.println("Enter first integer: ");
+      number1 = input.nextInt();
+
+      System.out.println("Enter second integer: ");
+      number2 = input.nextInt();
+
+      sum = number1 + number2;
+
+      System.out.printf("Sum is %d%n", sum);
+   }
+}
