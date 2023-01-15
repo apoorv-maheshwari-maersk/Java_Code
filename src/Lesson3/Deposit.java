dc916c7487ec14a4ea9b66417d8ac333391a2da2
apoorv-maheshwari-maersk <apoorv.maheshwari@maersk.com>
2023-01-16 02:39:17 +0530

    Deposit

---
+++ src/Lesson3/Deposit.java
@@ -0,0 +1,30 @@
+package Lesson3;
+
+public class Deposit {
+   private String name;
+   private double balance;
+
+   public Deposit(String name, double balance) {
+      this.name = name;
+      if(balance > 0.0)
+         this.balance = balance;
+   }
+
+   public void deposit(double depositAmount){
+      if(depositAmount > 0.0)
+         balance = balance + depositAmount;
+   }
+
+   public double getBalance(){
+      return balance;
+   }
+
+   public void setName(String name){
+      this.name =  name;
+   }
+
+   public String getName(){
+      return name;
+   }
+}
+
