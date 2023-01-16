5e6a1d6dd802c6e688dea0410205e3e5801e5880
apoorv-maheshwari-maersk <apoorv.maheshwari@maersk.com>
2023-01-16 23:29:25 +0530

    Do While

---
+++ src/Lesson5/DoWhile.java
@@ -0,0 +1,13 @@
+package Lesson5;
+
+public class DoWhile {
+   public static void main(String[] arg){
+      int counter = 1;
+      do{
+         System.out.printf("%d  ", counter);
+         ++counter;
+      } while (counter <=10);
+
+      System.out.println();
+   }
+}
